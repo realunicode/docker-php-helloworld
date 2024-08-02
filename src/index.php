8b0191aedf5177763639df811a3b2af07ca3b9bb
realunicode <robertlynntaylor276@gmail.com>
2024-08-02 10:23:30 +0000

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,13 +1,19 @@
 <?php
 
 $unique_id = $_GET['id'] ?? 'Unknown';
-$chat_id = $_GET['chat_id'] ?? '';
-$bot_token = $_GET['bot_token'] ?? '';
 
-$ip = $_SERVER['REMOTE_ADDR'];
-$timestamp = date('Y-m-d H:i:s');
+$log_file = "tracking_log_{$unique_id}.txt";
 
-if ($unique_id && $chat_id && $bot_token) {
+if (file_exists($log_file)) {
+    $log_content = file_get_contents($log_file);
+    
+    $lines = explode("\n", $log_content);
+    $chat_id = trim(explode(": ", $lines[0])[1]);
+    $bot_token = trim(explode(": ", $lines[1])[1]);
+
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $timestamp = date('Y-m-d H:i:s');
+
     $message = urlencode("Email with ID: $unique_id was opened at $timestamp from IP: $ip");
     $telegram_api_url = "https://api.telegram.org/bot$bot_token/sendMessage?chat_id=$chat_id&text=$message";
     file_get_contents($telegram_api_url);
