eebe9f68ddfe957fca47ab0ef0a6b973d1cde518
realunicode <robertlynntaylor276@gmail.com>
2024-08-02 10:10:06 +0000

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,7 +1,19 @@
 <?php
 
-echo "Hello, World from Docker! <br>";
-echo "Hello ECS! <br>";
-echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
+$unique_id = $_GET['id'] ?? 'Unknown';
+$chat_id = $_GET['chat_id'] ?? '';
+$bot_token = $_GET['bot_token'] ?? '';
+
+$ip = $_SERVER['REMOTE_ADDR'];
+$timestamp = date('Y-m-d H:i:s');
+
+if ($unique_id && $chat_id && $bot_token) {
+    $message = urlencode("Email with ID: $unique_id was opened at $timestamp from IP: $ip");
+    $telegram_api_url = "https://api.telegram.org/bot$bot_token/sendMessage?chat_id=$chat_id&text=$message";
+    file_get_contents($telegram_api_url);
+}
+
+header('Content-Type: image/gif');
+echo base64_decode('R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7');
 
 ?>
