b3e9de773ba00e268384e6638904f4b1708125e0
realunicode <robertlynntaylor276@gmail.com>
2024-08-02 12:02:39 +0000

    Update save.php

--- src/save.php
+++ src/save.php
@@ -7,11 +7,15 @@ $bot_token = $_GET['bot_token'] ?? null;
 if ($unique_id && $chat_id && $bot_token) {
     $log_file = "tracking_log_{$unique_id}.txt";
 
-    $log_content = "Chat ID: {$chat_id}\nBot Token: {$bot_token}\n";
+    if (file_exists($log_file)) {
+        echo "file exist";
+    } else {
+        $log_content = "Chat ID: {$chat_id}\nBot Token: {$bot_token}\n";
 
-    file_put_contents($log_file, $log_content);
+        file_put_contents($log_file, $log_content);
 
-    echo "Tracking information saved for ID: {$unique_id}";
+        echo "Tracking information saved for ID: {$unique_id}";
+    }
 } else {
     echo "Missing required parameters. Tracking information not saved.";
 }
