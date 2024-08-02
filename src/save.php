0b1a4644785cc9026591533cd2d3f8a8744c54a0
realunicode <robertlynntaylor276@gmail.com>
2024-08-02 12:35:12 +0000

    Update save.php

--- src/save.php
+++ src/save.php
@@ -1,6 +1,7 @@
 <?php
 
 $unique_id = $_GET['id'] ?? null;
+$recipient = $_GET['$recipient'] ?? null;
 $chat_id = $_GET['chat_id'] ?? null;
 $bot_token = $_GET['bot_token'] ?? null;
 
@@ -10,7 +11,7 @@ if ($unique_id && $chat_id && $bot_token) {
     if (file_exists($log_file)) {
         echo "file exist";
     } else {
-        $log_content = "Chat ID: {$chat_id}\nBot Token: {$bot_token}\n";
+        $log_content = "Recipient: {$recipient}\nChat ID: {$chat_id}\nBot Token: {$bot_token}\n";
 
         file_put_contents($log_file, $log_content);
 
